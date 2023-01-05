0ad2b580710135574283d770d21ca9084c58933a
Anirudh r k <menonani7@gmail.com>
2023-01-05 20:58:42 +0530

    Create pattern_assignment_day3.py

---
+++ pattern_assignment_day3.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    for j in range(5):
+        if i>=j:
+            print("*",end=' ')
+    print()
+for i in range(4):
+    for j in range(4):
+        if i<=j:
+            print("*",end=' ')
+    print()
