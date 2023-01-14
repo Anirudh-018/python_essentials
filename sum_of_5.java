46af9a3dbf142999337296c1e3150a5ef1189909
Anirudh r k <menonani7@gmail.com>
2023-01-14 22:57:19 +0530

    Create sum_of_5.java

---
+++ sum_of_5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sum_of_5{
+  public static void main(String[] args){
+    Scanner s=new Scanner(System.in);
+    int su=0
+    for(int i=1;i<6;i++)
+      su+=i
+    System.out.println(su);
+  }
+}
